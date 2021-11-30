02beef5dc034f175b20bacf7d92d35760e98338f
cg4g19 <cg4g19@soton.ac.uk>
2021-11-30 17:27:20 +0000

    Created OpenImaj maven project

---
+++ src/test/java/uk/ac/soton/ecs/group/AppTest.java
@@ -0,0 +1,18 @@
+package uk.ac.soton.ecs.group;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigourous Test :-)
+     */
+	@Test
+    public void testApp() {
+        assertTrue( true );
+    }
+}

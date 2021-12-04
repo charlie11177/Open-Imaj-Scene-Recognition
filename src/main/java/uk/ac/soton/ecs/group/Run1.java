95bee73f8d434737efb01bc0ca415ab77d9e237d
Isaac <iw3g19@soton.ac.uk>
2021-12-04 08:34:38 +0000

    Working classifier but not using KNNAnnotator Class so can be more concise i think?

--- src/main/java/uk/ac/soton/ecs/group/Run1.java
+++ src/main/java/uk/ac/soton/ecs/group/Run1.java
@@ -8,6 +8,8 @@ import org.openimaj.data.dataset.VFSGroupDataset;
 import org.openimaj.data.dataset.VFSListDataset;
 import org.openimaj.experiment.evaluation.classification.ClassificationEvaluator;
 import org.openimaj.experiment.evaluation.classification.analysers.confusionmatrix.CMAnalyser;
+import org.openimaj.feature.DoubleFVComparison;
+import org.openimaj.feature.FloatFV;
 import org.openimaj.feature.FloatFVComparison;
 import org.openimaj.image.DisplayUtilities;
 import org.openimaj.image.FImage;
@@ -15,22 +17,30 @@ import org.openimaj.image.ImageUtilities;
 import org.openimaj.image.MBFImage;
 import org.openimaj.image.colour.ColourSpace;
 import org.openimaj.image.colour.RGBColour;
+import org.openimaj.image.feature.FImage2FloatFV;
 import org.openimaj.image.processing.convolution.FGaussianConvolve;
 import org.openimaj.image.processing.resize.ResizeProcessor;
 import org.openimaj.image.typography.hershey.HersheyFont;
 import org.openimaj.knn.FloatNearestNeighboursExact;
 import org.openimaj.ml.annotation.basic.KNNAnnotator;
 import org.openimaj.util.comparator.DistanceComparator;
+import org.openimaj.util.pair.IntFloatPair;
 import org.openrdf.query.algebra.Str;
-
+import org.openimaj.image.annotation.evaluation.datasets.Caltech101.Record;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Run1 {
+    //TODO Figure out KNN implementation
+    //TODO Figure out how to output prediction file
     public static void main( String[] args ) {
         try {
-            GroupedDataset<String, VFSListDataset<FImage>, FImage> training = new VFSGroupDataset<FImage>(
+
+            // Data set imports
+            GroupedDataset<String, VFSListDataset<FImage>, FImage> training = new VFSGroupDataset(
                     "zip:http://comp3204.ecs.soton.ac.uk/cw/training.zip",
                     ImageUtilities.FIMAGE_READER);
 
@@ -38,100 +48,81 @@ public class Run1 {
                     "zip:http://comp3204.ecs.soton.ac.uk/cw/testing.zip",
                     ImageUtilities.FIMAGE_READER);
 
-            HashMap<Integer, FImage> map = new HashMap<>();
-            HashMap<Integer, String> classifications = new HashMap<>();
-            int index = 0;
-            for (String group : training.getGroups())
-            {
-                for (FImage image2 : training.get(group)) {
-                    // cropping code
-                    FImage image = image2.clone();
-                    int size1 = image.pixels.length;
-                    int size2 = image.pixels[0].length;
-                    if (size1 > size2) {
-                        float[][] cropped = new float[size2][size2];
-                        for (int i = 0; i < size2; i++) {
-                            for (int j = 0; j < size2; j++) {
-                                cropped[i][j] = image.pixels[i][j];
-                            }
-                        }
-                        image.pixels = cropped;
-                        if (image.pixels.length != image.pixels[0].length)
-                            System.out.println("Bruh");
-                    } else {
-                        float[][] cropped2 = new float[size1][size1];
-                        for (int i = 0; i < size1; i++) {
-                            for (int j = 0; j < size1; j++) {
-                                cropped2[i][j] = image.pixels[i][j];
-                            }
-                        }
-                        image.pixels = cropped2;
-                        if (image.pixels.length != image.pixels[0].length)
-                            System.out.println("Bruh");
-                    }
+            training.remove("training"); // Idk why but when doing a for each over the groups there's an extra group with everything
 
-//                    image = ResizeProcessor.zoomInplace(image, 16,16);
-                    map.put(index, image);
-                    classifications.put(index, group);
-                    index++;
-                }
-            }
-            for (FImage image : map.values()) {
-                if (image.pixels.length != image.pixels[0].length)
-                    System.out.println("DONT WORK");
-            }
 
-            ResizeProcessor.zoomInplace(map.get(1), 16,16);
+            // Training
 
-            System.out.println(map.size());
+            // Manual implementation
+//            HashMap<Integer, String> classificationMap = new HashMap<>();
+//            int trainingSize = 0;
+//            for (String s : training.getGroups()) {
+//                trainingSize += training.get(s).size();
+//            }
+//            float[][] imageVectors = new float[trainingSize][16 * 16];
+//
+//            int i = 0;
+//            for (String group : training.getGroups()) {
+//                System.out.println(training.get(group).size());
+//                for (FImage image : training.get(group)) {
+//                    imageVectors[i] = extractTinyImage(image).getFloatPixelVector();
+//                    classificationMap.put(i, group);
+//                    i++;
+//                }
+//            }
+//            FloatNearestNeighboursExact fNN = new FloatNearestNeighboursExact(imageVectors);
 
-            for (Integer num : map.keySet()) {
-                map.put(num, ResizeProcessor.resizeMax(map.get(num).clone(), 16));
-            }
+            // KNNAnnotator implementation
+            KNNAnnotator<FImage, String, FloatFV> classifier = new KNNAnnotator<>(new FImage2FloatFV(), FloatFVComparison.EUCLIDEAN);
 
-            for (FImage image : map.values()) {
-                System.out.println(image.pixels.length);
-                System.out.println(image.pixels[0].length);
-            }
 
-            // KNN stuff
+            // Prediction
 
-            float[][] combined = new float[training.size()][16*16];
-            int i = 0;
-            for (FImage image : map.values()) {
-                combined[i] = image.getFloatPixelVector();
-                i++;
-            }
-            FloatNearestNeighboursExact blah = new FloatNearestNeighboursExact(combined);
-//            blah.searchKNN(query, k);
 
         }
-        catch (Exception e)
-        {
+        catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public FImage cropImage(FImage image) {
-        int size1 = image.pixels.length;
-        int size2 = image.pixels[0].length;
-        if (size1 > size2) {
-            float[][] cropped = new float[size2][size2];
-            for (int i = 0; i < size2; i++) {
-                for (int j = 0; j < size2; j++) {
-                    cropped[i][j] = image.pixels[i][j];
-                }
-            }
-            return new FImage(cropped);
+    private static String classifyImage(FloatNearestNeighboursExact classifier, HashMap<Integer, String> mapping,FImage image) {
+        float[] query = extractTinyImage(image).getFloatPixelVector();
+        List<IntFloatPair> nearestNeighbours = classifier.searchKNN(query, 5);
+
+        List<String> neighbourClassifications = new ArrayList<>();
+        for (IntFloatPair pair : nearestNeighbours) {
+            neighbourClassifications.add(mapping.get(pair.first));
         }
-        else {
-            float[][] cropped2 = new float[size1][size1];
-            for (int i = 0; i < size1; i++) {
-                for (int j = 0; j < size1; j++) {
-                    cropped2[i][j] = image.pixels[i][j];
-                }
+
+        int mode = 0;
+        String modalValue = null;
+        for (String classification : neighbourClassifications) {
+            int count = 0;
+            for (String s : neighbourClassifications) {
+                if (s.equals(classification))
+                    count++;
             }
-            return new FImage(cropped2);
+            if (count > mode) {
+                mode = count;
+                modalValue = classification;
+            }
         }
+        return modalValue;
+    }
+
+    private static FImage extractTinyImage(FImage image) {
+
+//        System.out.println(image.width + " x " + image.height);
+
+
+        FImage out = new FImage(image.width, image.height);
+        if (image.width > image.height)
+            out = image.extractCenter(image.height, image.height);
+        else
+            out = image.extractCenter(image.width, image.width);
+
+//        System.out.println(out.width + " x " + out.height);
+
+        return ResizeProcessor.zoomInplace(out, 16,16);
     }
 }

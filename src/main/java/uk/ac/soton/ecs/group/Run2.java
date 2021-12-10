a8cb7e7ed97c74d726a559d03b2d8ada96949de9
cg4g19 <cg4g19@soton.ac.uk>
2021-12-10 14:12:49 +0000

    Assigner working

--- src/main/java/uk/ac/soton/ecs/group/Run2.java
+++ src/main/java/uk/ac/soton/ecs/group/Run2.java
@@ -1,51 +1,88 @@
 package uk.ac.soton.ecs.group;
 
 
+import de.bwaldvogel.liblinear.SolverType;
+import org.apache.commons.math3.geometry.euclidean.twod.Euclidean2D;
+import org.apache.commons.math3.ml.distance.EuclideanDistance;
 import org.apache.commons.vfs2.FileSystemException;
-import org.openimaj.data.DataSource;
-import org.openimaj.data.FloatArrayBackedDataSource;
 import org.openimaj.data.dataset.Dataset;
 import org.openimaj.data.dataset.VFSGroupDataset;
+import org.openimaj.experiment.evaluation.classification.ClassificationEvaluator;
+import org.openimaj.experiment.evaluation.classification.ClassificationResult;
+import org.openimaj.experiment.evaluation.classification.analysers.confusionmatrix.CMAnalyser;
+import org.openimaj.experiment.evaluation.classification.analysers.confusionmatrix.CMResult;
+import org.openimaj.feature.*;
 import org.openimaj.image.*;
-import org.openimaj.ml.clustering.FloatCentroidsResult;
+import org.openimaj.image.feature.local.aggregate.BagOfVisualWords;
+import org.openimaj.knn.ObjectNearestNeighbours;
+import org.openimaj.ml.annotation.linear.LiblinearAnnotator;
 import org.openimaj.ml.clustering.assignment.HardAssigner;
-import org.openimaj.ml.clustering.kmeans.FloatKMeans;
+import org.openimaj.ml.clustering.kmeans.ByteKMeans;
+import org.openimaj.ml.clustering.kmeans.FeatureVectorKMeans;
+import org.openimaj.ml.clustering.kmeans.KMeansConfiguration;
+import org.openimaj.util.comparator.DistanceComparator;
 import org.openimaj.util.pair.IntFloatPair;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class Run2 {
     public static void main( String[] args ) throws FileSystemException {
         VFSGroupDataset<FImage> training = new VFSGroupDataset<FImage>("zip:http://comp3204.ecs.soton.ac.uk/cw/training.zip", ImageUtilities.FIMAGE_READER);
         VFSGroupDataset<FImage> testing = new VFSGroupDataset<FImage>("zip:http://comp3204.ecs.soton.ac.uk/cw/testing.zip", ImageUtilities.FIMAGE_READER);
 
-        HardAssigner<float[], float[], IntFloatPair> assigner = trainQuantiser(training);
+        HardAssigner<DoubleFV, float[], IntFloatPair> assigner = trainQuantiser(training);
+
+        FeatureExtractor<SparseIntFV, FImage> extractor = new POWExtractor(assigner);
+
+        LiblinearAnnotator<FImage, String> ann = new LiblinearAnnotator<FImage, String>(
+                extractor, LiblinearAnnotator.Mode.MULTICLASS, SolverType.L2R_L2LOSS_SVC, 1.0, 0.00001);
+        ann.train(training);
+
+        ClassificationEvaluator<CMResult<String>, String, FImage> eval =
+                new ClassificationEvaluator<CMResult<String>, String, FImage>(
+                        ann, testing, new CMAnalyser<FImage, String>(CMAnalyser.Strategy.SINGLE));
+
+        Map<FImage,ClassificationResult<String>> guesses = eval.evaluate();
+        System.out.println(guesses.size());
+        CMResult<String> result = eval.analyse(guesses);
     }
 
 
-    static HardAssigner<float[], float[], IntFloatPair> trainQuantiser(Dataset<FImage> sample) {
-        List<List<float[]>> allPatches = new ArrayList<List<float[]>>();
+    static HardAssigner<DoubleFV, float[], IntFloatPair> trainQuantiser(Dataset<FImage> sample) {
+        List<DoubleFV> allPatches = new ArrayList<DoubleFV>();
 
         for (FImage image : sample) {
-            List<float[]> patches = new PatchExtractor(image).extractPatches();
-            allPatches.add(patches);
+            List<DoubleFV> patches = new PatchExtractor(image).extractFeatureVectors();
+            allPatches.addAll(patches);
         }
 
         if (allPatches.size() > 10000)
             allPatches = allPatches.subList(0, 10000);
 
-        FloatKMeans cluster = FloatKMeans.createKDTreeEnsemble(500);
+        FeatureVectorKMeans<DoubleFV> km = FeatureVectorKMeans.createExact(500, DoubleFVComparison.EUCLIDEAN);
+        FeatureVectorKMeans.Result<DoubleFV> result = km.cluster(allPatches);
 
-        //TODO: Convert List<List<float[]>> to float[][]
-        // Main issue is the patches: do we need them as float[], or can we convert them to float ??
-        // what is the point of mean-centering a patch (an array) and normalising ????
-
-        // FloatArrayBackedDataSource needs a float[][]
-        DataSource<float[]> dataSource = new FloatArrayBackedDataSource(allPatches);
-        FloatCentroidsResult result = cluster.cluster(dataSource);
+        //TODO: Now ended up with List<DoubleFV>
+        // Ive created a method which turns an image into a list of DoubleFV hopefully we can use KMeans easier
+        // what is the point of mean-centering a patch (an array) and normalising ???? No idea spec just said we might want to try it
 
         return result.defaultHardAssigner();
+    }
 
+    static class POWExtractor implements FeatureExtractor<SparseIntFV, FImage> {
+        HardAssigner<DoubleFV, float[], IntFloatPair> assigner;
+
+        public POWExtractor(HardAssigner<DoubleFV, float[], IntFloatPair> assigner){
+            this.assigner = assigner;
+        }
+
+        @Override
+        public SparseIntFV extractFeature(FImage image) {
+            BagOfVisualWords<DoubleFV> bovw = new BagOfVisualWords<DoubleFV>(assigner);
+            return bovw.aggregateVectorsRaw(new PatchExtractor(image).extractFeatureVectors());
+
+        }
     }
 }
